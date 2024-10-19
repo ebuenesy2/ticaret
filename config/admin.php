c254bdce9e1c0de2915d446c8e02028851b53e41
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2024-10-19 15:22:52 +0300

    ok

--- config/admin.php
+++ config/admin.php
@@ -1,54 +1,54 @@
 <?php
 
    return [
-     'Default_Lang' => 'tr',
+    'Default_Lang' => 'tr',
 
-     'CompanyTitle' => 'Interturk A.Ş',
-     'CompanyDescription' => 'Interturk A.Ş Admin & CMS & Web Site Yönetim',
-     'CompanyLogo' => '/assets/img/company/interturk.png',
+    'CompanyTitle' => 'YıldırımDev',
+    'CompanyDescription' => 'YıldırımDev Admin & CMS & Web Site Yönetim',
+    'CompanyLogo' => '/assets/img/company/index.png',
 
-     'Web_Title' => 'Ticaret Uygulaması',
-    //  'Web_Url' => 'http://localhost:8000',
-     'Web_Url' => 'https://www.eximpro.com.tr',
+    'Web_Title' => 'Ticaret Uygulaması',
+    //'Web_Url' => 'http://localhost:8000',
+    'Web_Url' => 'https://www.yildirimdev.com',
 
-     'Api_Url' => 'http://localhost:3002',
-     'Socket_Url' => 'Socket Adresi',
+    'Api_Url' => 'http://localhost:3002',
+    'Socket_Url' => 'Socket Adresi',
 
-     'ServerId' => 3,
-     'ServerToken' => "serverToken2",
+    'ServerId' => 3,
+    'ServerToken' => "serverToken2",
 
-     'CompanySetting' => 'İnterturk Yapı Malzemeleri Tekstil Gıda Sanayi İç ve Dış Ticaret Anonim Şirketi',
-     'CompanyAdresi' => 'Çukurambar, Mevlana Blv. No: 150 Daire: 121 06530 Çankaya / Ankara / Türkiye',
-     'CompanyAuthorizedPerson' => 'Kerim SARGIN',
-     'CompanyAuthorizedTel' => '+90 312 2853888',
-     'CompanyAuthorizedMail' => 'info@interturk.com.tr',
-     'companyAuthorized_person_tax_no' => '148745477775',
-     'companyAuthorized_person_tax_adm' => 'ÇukurAmbar',
+    'CompanySetting' =>  'YıldırımDev Admin',
+    'CompanyAdresi' => 'Keçiören / Ankara / Türkiye',
+    'CompanyAuthorizedPerson' => 'Ebu Enes Yıldırım',
+    'CompanyAuthorizedTel' => '+90 312 2xx xx',
+    'CompanyAuthorizedMail' => 'ebuenesy2@gmail.com',
+    'companyAuthorized_person_tax_no' => '899555',
+    'companyAuthorized_person_tax_adm' => 'Kızılay',
 
-     'Admin_Title' => 'Dış Ticaret',
-     'Admin_Meta_Title' => 'Interturk Admin Sayfası',
-     'Admin_Meta_Author' => 'Interturk',
-     'Admin_Meta_Description' => 'Interturk',
-     'Admin_Keywords' => 'Interturk',
+    'Admin_Title' => 'YıldırımDev Admin',
+    'Admin_Meta_Title' => 'YıldırımDev Admin Sayfası',
+    'Admin_Meta_Author' => 'YıldırımDev - Ebu Enes Yıldırım',
+    'Admin_Meta_Description' => 'YıldırımDev den tarafından yapılmış. Site kontrollerini barındıran sistemdir.',
+    'Admin_Keywords' => 'YıldırımDev, yildirim, kod, ebu, enes, mühendis, admin, tema',
 
-     'Admin_Logo' => array(
-      'dark' => array(
-        'url'=>'/admin',
-        'sm' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo dark sm' ),
-        'lg' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo dark lg' ),
-      ),
-      'light' => array(
-        'url'=>'/admin',
-        'sm' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo light sm' ),
-        'lg' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo light lg' ),
-      ),
-      
-     ),
+    'Admin_Logo' => array(
+    'dark' => array(
+      'url'=>'/admin',
+      'sm' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo dark sm' ),
+      'lg' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo dark lg' ),
+    ),
+    'light' => array(
+      'url'=>'/admin',
+      'sm' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo light sm' ),
+      'lg' => array('url'=>'#', 'imgUrl'=> '/img/company/index.png' , 'imgAltDescription' => 'Interturk admin logo light lg' ),
+    ),
+    
+    ),
 
-     'Default_UserImgUrl' => '/assets/img/user/default.jpg',
-     'Default_ProductImgUrl' => '/assets/img/product/default.jpg',
+    'Default_UserImgUrl' => '/assets/img/user/default.jpg',
+    'Default_ProductImgUrl' => '/assets/img/product/default.jpg',
 
-     'CompanyName' => 'İntertürk İç ve Dış Ticaret A.Ş.',
+    'CompanyName' => 'İntertürk İç ve Dış Ticaret A.Ş.',
      
      
    ]
