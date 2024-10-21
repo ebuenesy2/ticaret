73ff3a321f4444be9cfdd04fd40e919094f4561e
EBU ENES YILDIRIM <86044516+ebuenesy2@users.noreply.github.com>
2024-10-21 06:06:44 +0300

    ok

--- public/assets/admin/js/proforma/proforma_invoice_list.js
+++ public/assets/admin/js/proforma/proforma_invoice_list.js
@@ -498,26 +498,26 @@ var paginationNext = (ContactList && (contactList = new List("contactList", opti
     $("#new_add").click(function (e) {
         e.preventDefault();
 
-        var public_Val = $('#VisibilityAdd').val();
-        var CurrentId_Val = $('#selectCurrentCartAdd').val();
+        var TitleAdd = $('#TitleAdd').val();
+        var selectCalculationAdd = $('#selectCalculationAdd').val();
 
-        if(public_Val == "") {
+        if(TitleAdd == "") {
 
             Swal.fire({
                 position: "center",
                 icon: "error",
-                title: $('[id=lang_change][data_key=PrivacyNotselected]').html().trim(),
+                title: "Başlık Yazılmadı",
                 showConfirmButton: false,
                 timer: 2000,
             });
 
         }
-        else  if(CurrentId_Val == "") {
+        else if(selectCalculationAdd == "") {
 
             Swal.fire({
                 position: "center",
                 icon: "error",
-                title: $('[id=lang_change][data_key=CurrentIDNotWritten]').html().trim(),
+                title: "Maliyet Seçilmedi",
                 showConfirmButton: false,
                 timer: 2000,
             });
@@ -534,7 +534,6 @@ var paginationNext = (ContactList && (contactList = new List("contactList", opti
                     siteLang: $('[id=lang_change][data_key=lang]').html().trim(),
                     title: $('#TitleAdd').val(),
                     cost_calculation_id: $('#selectCalculationAdd').val(),
-                    public: $('#VisibilityAdd').val(),
                     created_byId: document.cookie.split(';').find((row) => row.startsWith(' yildirimdev_userID='))?.split('=')[1]
                 },
                 success: function (response) {
@@ -579,8 +578,6 @@ var paginationNext = (ContactList && (contactList = new List("contactList", opti
 
     }); //! Ekleme Son
     //! ************ Ekleme Son  ***************
-
-
 
     //! ************ Silme  ***************
     document.querySelectorAll("#listItemDelete").forEach(function (i) {
@@ -729,122 +726,4 @@ var paginationNext = (ContactList && (contactList = new List("contactList", opti
     }); //! Aktif Son
     //! ************ Aktif Son ***************
 
-
-
-
  
-    
-
-
- 
-
-
-    //! ************ Import ***************
-
-    //! FileUpload
-    $("#fileUploadClick").click(function (e) {
-        e.preventDefault();
-        //alert("fileUploadClick");
-
-        //! Dosya Yükleme
-        const fileInput = document.querySelector("#fileInput");
-        const fileInputFiles = fileInput.files;
-        //console.log("fileInputFiles:",fileInputFiles);
-        
-
-        //! Yeni Form Veriler
-        var formData = new FormData();
-        formData.append("file", fileInputFiles[0]);
-        formData.append("fileDbSave", $('#fileDbSave').val());
-        formData.append("fileWhere", $('#fileWhere').val());
-
-        $.ajax({
-            xhr: function() {
-                var xhr = new window.XMLHttpRequest();
-                xhr.upload.addEventListener("progress", function(evt) {
-                    if (evt.lengthComputable) {
-                        var percentComplete = ((evt.loaded / evt.total) * 100);
-                        console.log("Dosya Yükleme Durumu: %", percentComplete);
-                        
-                        $("#progressBarFileUpload").width(percentComplete + '%');
-                        $("#progressBarFileUpload").html(percentComplete+'%');
-                    }
-                }, false);
-                return xhr;
-            },
-            url: "/sabit_list/export/file",
-            method: "post",
-            headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-            data: formData,
-            contentType: false,
-            cache: false,
-            processData:false,
-            beforeSend: function () {
-                console.log("Dosya yükleme başladı");
-
-                //! ProgressBar
-                $("#progressBarFileUpload").width('0%');
-
-                //! Upload Durum
-                $('#LoadingFileUpload').toggle();
-                $('#uploadStatus').hide();
-
-                //! Upload Url
-                $('#filePathUrl').html("");
-            },
-            error: function (error) {
-                alert("başarısız");
-                console.log("Hata oluştu error:", error);
-
-                //! Upload Durum
-                $('#LoadingFileUpload').hide();
-                $('#uploadStatus').html('<p style="color:#EA4335;">File upload failed, please try again.</p>');
-
-                //! Upload Url
-                $('#filePathUrl').html("");
-
-                //! Alert
-                Swal.fire({
-                    position: "center",
-                    icon: "error",
-                    title: $('[id=lang_change][data_key=TransactionFailed]').html().trim(),
-                    showConfirmButton: false,
-                    timer: 2000,
-                });  //! Alert Son
-
-            },
-            success: function (resp) {
-                //alert("Başarılı");
-                console.log("file resp:", resp);
-
-                //! ProgressBar
-                $("#progressBarFileUpload").width('100%');
-
-                //! Upload Durum
-                $('#LoadingFileUpload').hide();
-                $('#uploadStatus').hide();
-
-                //! Upload Url
-                $('#filePathUrl').html(resp.file_url);
-
-                //! Alert
-                Swal.fire({
-                    position: "center",
-                    icon: "success",
-                    title: $('[id=lang_change][data_key=TransactionSuccessful]').html().trim(),
-                    showConfirmButton: false,
-                    timer: 2000,
-                });  //! Alert Son
-
-                //! Sayfa Yenileme
-                if(resp.DB_importStatus) { window.location.reload(); }
-
-            }
-        }); //! Ajax
-
-
-
-    });
-    //! FileUpload Son
-
-    //! ************ Import  Son ***************
